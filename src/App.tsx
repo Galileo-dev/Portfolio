931c142c4f96174856ada8c7637a563dc11f7f73
Galileo <Galileo-dev@users.noreply.github.com>
2021-05-30 22:20:32 +0100

    Test new production release

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
 const StyledButton = styled.button`
   font-size: calc(10px + 2vmin);
-  background: ${({ theme }) => theme.ButtonNormal};
+  background: red;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 16px;
   border-style: none;
