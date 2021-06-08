5bd4add5033a42503f82902a260d4ec2f6cfbd70
Galileo-dev <Galileo-dev@users.noreply.github.com>
2021-06-08 20:30:25 +0000

    Prettified Code!

--- src/Theme.js
+++ src/Theme.js
@@ -7,38 +7,38 @@
 exports.__esModule = true;
 exports.LightTheme = void 0;
 exports.LightTheme = {
-    light: {
-        id: "T_001",
-        name: "Light",
-        colors: {
-            body: "#FFFFFF",
-            text: "#000000",
-            button: {
-                text: "#FFFFFF",
-                background: "#000000"
-            },
-            link: {
-                text: "teal",
-                opacity: 1
-            }
-        },
-        font: "Tinos"
+  light: {
+    id: "T_001",
+    name: "Light",
+    colors: {
+      body: "#FFFFFF",
+      text: "#000000",
+      button: {
+        text: "#FFFFFF",
+        background: "#000000",
+      },
+      link: {
+        text: "teal",
+        opacity: 1,
+      },
     },
-    seaWave: {
-        id: "T_007",
-        name: "Sea Wave",
-        colors: {
-            body: "#9be7ff",
-            text: "#0d47a1",
-            button: {
-                text: "#ffffff",
-                background: "#0d47a1"
-            },
-            link: {
-                text: "#0d47a1",
-                opacity: 0.8
-            }
-        },
-        font: "Ubuntu"
-    }
+    font: "Tinos",
+  },
+  seaWave: {
+    id: "T_007",
+    name: "Sea Wave",
+    colors: {
+      body: "#9be7ff",
+      text: "#0d47a1",
+      button: {
+        text: "#ffffff",
+        background: "#0d47a1",
+      },
+      link: {
+        text: "#0d47a1",
+        opacity: 0.8,
+      },
+    },
+    font: "Ubuntu",
+  },
 };
--- src/app.js
+++ src/app.js
@@ -1,19 +1,29 @@
 "use strict";
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+var __makeTemplateObject =
+  (this && this.__makeTemplateObject) ||
+  function (cooked, raw) {
+    if (Object.defineProperty) {
+      Object.defineProperty(cooked, "raw", { value: raw });
+    } else {
+      cooked.raw = raw;
+    }
     return cooked;
-};
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
+  };
+var __assign =
+  (this && this.__assign) ||
+  function () {
+    __assign =
+      Object.assign ||
+      function (t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
+          s = arguments[i];
+          for (var p in s)
+            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
         }
         return t;
-    };
+      };
     return __assign.apply(this, arguments);
-};
+  };
 exports.__esModule = true;
 var react_1 = require("react");
 var styled_components_1 = require("styled-components");
@@ -24,40 +34,113 @@ var drei_1 = require("@react-three/drei");
 var react_dat_gui_1 = require("react-dat-gui");
 require("../node_modules/react-dat-gui/dist/index.css");
 function App(props) {
-    // State:
-    var _a = react_1.useState([0, 0, 0, 0]), rotation = _a[0], setRotation = _a[1];
-    var _b = react_1.useState({
-        isDev: false,
-        roughness: 0.1,
-        metalness: 0.5,
-        opacity: 1,
-        color: "#fff"
-    }), parms = _b[0], setParms = _b[1];
-    return (react_1["default"].createElement(styled_components_1.ThemeProvider, { theme: Theme_1.LightTheme },
-        react_1["default"].createElement(fiber_1.Canvas, { camera: { fov: 75, position: [0, 0, 12] }, gl: { antialias: true } },
-            react_1["default"].createElement("color", { attach: "background", args: ["#da6c19"] }),
-            react_1["default"].createElement("directionalLight", { position: [-40, 20, 20], color: "#ffffff" }),
-            react_1["default"].createElement("directionalLight", { position: [10.5, 20, 10], intensity: 1.5, color: "#e78f48" }),
-            react_1["default"].createElement("mesh", null,
-                react_1["default"].createElement("boxBufferGeometry", null),
-                react_1["default"].createElement("meshStandardMaterial", { attach: "material", color: parms.color, transparent: true, opacity: parms.opacity, roughness: parms.roughness, metalness: parms.metalness })),
-            react_1["default"].createElement("ambientLight", { color: "#8d69cb" }),
-            react_1["default"].createElement(react_1.Suspense, { fallback: null },
-                react_1["default"].createElement(drei_1.Environment, { preset: "night" })),
-            parms.isDev ? react_1["default"].createElement(drei_1.OrbitControls, __assign({}, props)) : ""),
-        react_1["default"].createElement(react_dat_gui_1["default"], { data: parms, onUpdate: setParms },
-            react_1["default"].createElement(react_dat_gui_1.DatBoolean, { path: "isDev" }),
-            react_1["default"].createElement(react_dat_gui_1.DatNumber, { path: "roughness", min: 0, max: 1, step: 0.01 }),
-            react_1["default"].createElement(react_dat_gui_1.DatNumber, { path: "metalness", min: 0, max: 1, step: 0.01 }),
-            react_1["default"].createElement(react_dat_gui_1.DatColor, { path: "color" }))));
+  // State:
+  var _a = react_1.useState([0, 0, 0, 0]),
+    rotation = _a[0],
+    setRotation = _a[1];
+  var _b = react_1.useState({
+      isDev: false,
+      roughness: 0.1,
+      metalness: 0.5,
+      opacity: 1,
+      color: "#fff",
+    }),
+    parms = _b[0],
+    setParms = _b[1];
+  return react_1["default"].createElement(
+    styled_components_1.ThemeProvider,
+    { theme: Theme_1.LightTheme },
+    react_1["default"].createElement(
+      fiber_1.Canvas,
+      { camera: { fov: 75, position: [0, 0, 12] }, gl: { antialias: true } },
+      react_1["default"].createElement("color", {
+        attach: "background",
+        args: ["#da6c19"],
+      }),
+      react_1["default"].createElement("directionalLight", {
+        position: [-40, 20, 20],
+        color: "#ffffff",
+      }),
+      react_1["default"].createElement("directionalLight", {
+        position: [10.5, 20, 10],
+        intensity: 1.5,
+        color: "#e78f48",
+      }),
+      react_1["default"].createElement(
+        "mesh",
+        null,
+        react_1["default"].createElement("boxBufferGeometry", null),
+        react_1["default"].createElement("meshStandardMaterial", {
+          attach: "material",
+          color: parms.color,
+          transparent: true,
+          opacity: parms.opacity,
+          roughness: parms.roughness,
+          metalness: parms.metalness,
+        })
+      ),
+      react_1["default"].createElement("ambientLight", { color: "#8d69cb" }),
+      react_1["default"].createElement(
+        react_1.Suspense,
+        { fallback: null },
+        react_1["default"].createElement(drei_1.Environment, {
+          preset: "night",
+        })
+      ),
+      parms.isDev
+        ? react_1["default"].createElement(
+            drei_1.OrbitControls,
+            __assign({}, props)
+          )
+        : ""
+    ),
+    react_1["default"].createElement(
+      react_dat_gui_1["default"],
+      { data: parms, onUpdate: setParms },
+      react_1["default"].createElement(react_dat_gui_1.DatBoolean, {
+        path: "isDev",
+      }),
+      react_1["default"].createElement(react_dat_gui_1.DatNumber, {
+        path: "roughness",
+        min: 0,
+        max: 1,
+        step: 0.01,
+      }),
+      react_1["default"].createElement(react_dat_gui_1.DatNumber, {
+        path: "metalness",
+        min: 0,
+        max: 1,
+        step: 0.01,
+      }),
+      react_1["default"].createElement(react_dat_gui_1.DatColor, {
+        path: "color",
+      })
+    )
+  );
 }
-var StyledButton = styled_components_1["default"].button(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  font-size: calc(10px + 2vmin);\n  background: red;\n  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);\n  border-radius: 16px;\n  border-style: none;\n  padding: 10px 20px;\n  transition: 0.1s;\n  &:hover {\n    background: ", ";\n  }\n  &:active {\n    transform: scale(0.97);\n    background-color: ", ";\n  }\n"], ["\n  font-size: calc(10px + 2vmin);\n  background: red;\n  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);\n  border-radius: 16px;\n  border-style: none;\n  padding: 10px 20px;\n  transition: 0.1s;\n  &:hover {\n    background: ", ";\n  }\n  &:active {\n    transform: scale(0.97);\n    background-color: ", ";\n  }\n"])), function (_a) {
+var StyledButton = styled_components_1["default"].button(
+  templateObject_1 ||
+    (templateObject_1 = __makeTemplateObject(
+      [
+        "\n  font-size: calc(10px + 2vmin);\n  background: red;\n  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);\n  border-radius: 16px;\n  border-style: none;\n  padding: 10px 20px;\n  transition: 0.1s;\n  &:hover {\n    background: ",
+        ";\n  }\n  &:active {\n    transform: scale(0.97);\n    background-color: ",
+        ";\n  }\n",
+      ],
+      [
+        "\n  font-size: calc(10px + 2vmin);\n  background: red;\n  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);\n  border-radius: 16px;\n  border-style: none;\n  padding: 10px 20px;\n  transition: 0.1s;\n  &:hover {\n    background: ",
+        ";\n  }\n  &:active {\n    transform: scale(0.97);\n    background-color: ",
+        ";\n  }\n",
+      ]
+    )),
+  function (_a) {
     var theme = _a.theme;
     return theme.ButtonNormalHover;
-}, function (_a) {
+  },
+  function (_a) {
     var theme = _a.theme;
     return theme.ButtonNormalActive;
-});
+  }
+);
 // const GUI = (props: any) => {
 //   const { opts, setOpts } = props;
 //   return (
